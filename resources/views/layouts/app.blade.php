f422a16b4b3af6e01a3624199bc3221ce91fae49
Iqrash122 <xagent891@gmail.com>
2026-05-09 15:23:39 +0500

    'iqrash

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -245,13 +245,13 @@
                     Generations
                 </a>
 
-                <a href="{{ route('explore-feed.index') }}"
+                <!-- <a href="{{ route('explore-feed.index') }}"
                     class="sidebar-link {{ request()->routeIs('explore-feed.*') ? 'active' : '' }}">
                     <svg class="sidebar-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"></path>
                     </svg>
                     Explore Feed
-                </a>
+                </a> -->
             </nav>
 
             {{-- User Footer --}}
